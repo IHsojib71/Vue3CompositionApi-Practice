88bbfb1e9cf7865c5a4384eca277463d268f70f6
Md Ismail Hossain <ihsojib43@gmail.com>
2024-05-22 18:45:46 +0600

    vue 3 app with vue router added, event card component added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,79 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+
+const events = ref([
+  {
+  id: 4544,
+  category: 'Laptop Flash Sale',
+  title: 'Macbook Pro 2021',
+  price: 1200,
+  brand: 'Apple',
+  time:'12:30',
+  date: '10 June, 2024',
+
+  },
+
+  {
+  
+  id: 4555,
+  category: 'Laptop Flash Sale',
+  title: 'Macbook Pro 2021',
+  price: 1200,
+  brand: 'Apple',
+  time:'12:30',
+  date: '10 June, 2024',
+
+  },
+  {
+  
+  id: 4534,
+  category: 'Laptop Flash Sale',
+  title: 'Macbook Pro 2021',
+  price: 1200,
+  brand: 'Apple',
+  time:'12:30',
+  date: '10 June, 2024',
+
+  },
+  {
+  
+  id: 4344,
+  category: 'Laptop Flash Sale',
+  title: 'Macbook Pro 2021',
+  price: 1200,
+  brand: 'Apple',
+  time:'12:30',
+  date: '10 June, 2024',
+
+  },
+  {
+  
+  id: 4244,
+  category: 'Laptop Flash Sale',
+  title: 'Macbook Pro 2021',
+  price: 1200,
+  brand: 'Apple',
+  time:'12:30',
+  date: '10 June, 2024',
+
+  },
+
+])
+</script>
+
+<template>
+  <div class="events">
+  <h1>My Events</h1>
+    <EventCard v-for="(event,index) in events" :key="index" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+  .events{
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+  }
+
+</style>
